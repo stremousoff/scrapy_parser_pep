912fbd45476e7cea8f2a71a830fe7788ae3607b2
Anton Stremousov <stremousoff@gmail.com>
2024-09-25 00:07:57 +0300

    done

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -7,28 +7,18 @@ from pep_parse.constants import BASE_DIR, UtilityConstants
 
 class PepParsePipeline:
 
-    # я был за этот вариант, но тесты свели меня с ума, что то не то с __init__
-    # def __init__(self):
-    #     self.result = defaultdict(int)
-    #     self.results_dir = BASE_DIR / UtilityConstants.RESULTS_DIR
-    #     self.results_dir.mkdir(exist_ok=True)
-
     def __init__(self):
         self.result = defaultdict(int)
 
-    def open_spider(self, spider):  # строки 13 и 14 здесь разместить тоже
-        # тоже нельзя, валятся тесты на платформе
-        pass
+    def open_spider(self, spider):
+        self.results_dir = BASE_DIR / UtilityConstants.RESULTS_DIR
+        self.results_dir.mkdir(exist_ok=True)
 
     def process_item(self, item, spider):
         self.result[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        # строки 30-31 - только так тесты проходят на платформе, на локалке всё
-        # работает и с __init__ что закоменчен
-        self.results_dir = BASE_DIR / UtilityConstants.RESULTS_DIR
-        self.results_dir.mkdir(exist_ok=True)
         with open(
                 self.results_dir / '{}{}.csv'.format(
                     UtilityConstants.FILE_NAME,
